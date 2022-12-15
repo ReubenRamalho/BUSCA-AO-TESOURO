fd42c521eb39d0b6c9fb76792785845bd634e77f
ReubenRamalho <reubenramalho@gmail.com>
2022-12-14 23:01:36 -0300

    Update

--- src/model/Tabuleiro.java
+++ src/model/Tabuleiro.java
@@ -1,11 +1,5 @@
 package model;
-/**
- * O Tabuleiro do jogo. 
- * Respons�vel por armazenar as 64 casas.
- * 
- * @author Alan Moraes &lt;alan@ci.ufpb.br&gt;
- * @author Leonardo Villeth &lt;lvilleth@cc.ci.ufpb.br&gt;
- */
+
 public class Tabuleiro {
 
     private Casa[][] board;
